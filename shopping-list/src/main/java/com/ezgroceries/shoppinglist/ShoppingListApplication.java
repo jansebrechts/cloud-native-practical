7761678a089a384cf36adb75b20f7368493769f4
Jan Sebrechts <jan.sebrechts@kbc.be>
2019-05-20 14:06:42 +0200

    setup swagger

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -1,11 +1,14 @@
 package com.ezgroceries.shoppinglist;
 
+import com.ezgroceries.shoppinglist.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@Import(SwaggerConfig.class)
 public class ShoppingListApplication {
 
     public static void main(String[] args) {
